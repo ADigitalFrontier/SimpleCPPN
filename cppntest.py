6dd1fa0f8e717464b456d4a35dcfafaae466c909
Aaron <abot1998@gmail.com>
2022-09-01 00:14:50 -0400

    ex machina moment

--- cppntest.py
+++ cppntest.py
@@ -23,11 +23,15 @@ def make_cppn(min_hnodes, max_hnodes, min_connections, max_connections):
         hnode['activation'] = activation
         hnode['label'] = activation.__name__
 
-    brightness = cppn.add_node(node_type="output", position=(0.5, -1), color="green")
-    brightness['label'] = "Brightness"
+    r = cppn.add_node(node_type="output", position=(0, -1), color="green")
+    r['label'] = "R"
+    g = cppn.add_node(node_type="output", position=(.5, -1), color="green")
+    g['label'] = "G"
+    b = cppn.add_node(node_type="output", position=(1, -1), color="green")
+    b['label'] = "B"
 
     inputs = [x, y]
-    outputs = [brightness]
+    outputs = [r,g,b]
 
     node_pool_a = inputs + hnodes
     node_pool_b = hnodes + outputs
@@ -43,27 +47,40 @@ def make_cppn(min_hnodes, max_hnodes, min_connections, max_connections):
 
     return cppn
 
+for i in range(500):
+    cppn = make_cppn(3, 15, 3, 15)
+    # if there is no path from any of the inputs to any of the outputs, skip this cppn
+    is_valid = False
+    for inode in cppn.inputs:
+        for onode in cppn.outputs:
+            if nx.has_path(cppn.graph, inode, onode):
+                is_valid = True
+                break
 
-cppn = make_cppn(3, 10, 3, 10)
-cppn.show()
+    if not is_valid:
+        print("Invalid cppn, skipping")
+        continue
 
-num_x = 100
-num_y = 100
+    num_x = 250
+    num_y = 250
 
-# generate image of size num_x x num_y
-image = Image.new("RGB", (num_x, num_y))
+    # generate image of size num_x x num_y
+    image = Image.new("RGB", (num_x, num_y))
 
-for i in range(num_y):
-    y = i/num_x
-    row = []
-    for j in range(num_x):
-        x = j/num_y
-        # print("X: " + str(x), "Y: " + str(y))
-        cppn.graph.nodes[1]['value'] = x
-        cppn.graph.nodes[2]['value'] = y
-        out = cppn.evaluate()
-        color = int(abs(out[0]) * 255)
-        # set the pixel to the color
-        image.putpixel((j, i), (color, color, color))
+    for i in range(num_y):
+        y = i/num_x
+        row = []
+        for j in range(num_x):
+            x = j/num_y
+            # print("X: " + str(x), "Y: " + str(y))
+            cppn.graph.nodes[1]['value'] = x
+            cppn.graph.nodes[2]['value'] = y
+            out = cppn.evaluate()
+            red = int(abs(out[0]) * 255)
+            green = int(abs(out[1]) * 255)
+            blue = int(abs(out[2]) * 255)
+            # set the pixel to the color
+            image.putpixel((j, i), (red, green, blue))
 
-image.show()
+    # set the image to the 
+    image.show()
