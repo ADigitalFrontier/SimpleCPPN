3a91ee5939a593ec6199f0133f8d7a6adb29fd7c
Aaron <abot1998@gmail.com>
2022-09-03 16:24:53 -0400

    added modify image option

--- mutate_test.py
+++ mutate_test.py
@@ -8,7 +8,7 @@ from make_cppn import load_cppn, make_cppn
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw 
-from make_image import make_image, make_image_with_output_coords
+from make_image import make_image, make_image_with_output_coords, run_cppn
 from PIL import ImageFont
 from PIL import ImageDraw
 import os
@@ -31,16 +31,19 @@ NUM_HIDDEN = 24
 NUM_CONNECTIONS = 42
 LABEL_FONT = 144
 STABILIZATION_AGE = 20
-ADD_NODE_AMOUNT = 8
-ADD_EDGE_AMOUNT = 8
+ADD_NODE_AMOUNT = 15
+ADD_EDGE_AMOUNT = 15
 EDGE_MUTATE_RATE = .125
 NODE_MUTATE_RATE = .1
 
 MAP_OUTPUT_COORDS = False
+MODIFY_IMAGE = True
+
+TEST_IMAGE = "C:/Users/Aaron/Desktop/test.jpg"
 
 PROB_TABLE = {
     "edge": {
-        "remove": 0.3,
+        "remove": .1,
         "change": .7
     },
     "node": {
@@ -48,7 +51,7 @@ PROB_TABLE = {
         "remove": 0.3,
         "change": .5
     },
-    "intercept_rate": .7,
+    "intercept_rate": .6,
 }
 
 current_path = ""
@@ -57,6 +60,33 @@ keep_going = True
 current_dir = SAVE_DIR
 current_cppn = ORIGINAL
 
+
+inputs = []
+if MODIFY_IMAGE:
+    # scale down the image so the highest dimension is 64
+    image = Image.open(TEST_IMAGE)
+    # get x and y dimensions
+    x_dim = image.size[0]
+    y_dim = image.size[1]
+
+    # get highest dimension
+    if x_dim > y_dim:
+        max_dim = x_dim
+        new_x = 64
+        new_y = int((y_dim/x_dim) * 64)
+    else:
+        max_dim = y_dim
+        new_y = 64
+        new_x = int((x_dim/y_dim) * 64)
+
+    image = image.resize((new_x, new_y), Image.ANTIALIAS)
+    # for each pixel in the image, get the RGB values
+    for x in range(new_x):
+        for y in range(new_y):
+            r, g, b = image.getpixel((x, y))
+            inputs.append([x/new_x, y/new_y, r, g, b])
+
+
 # get the number of files in the current directory
 num_iterations = 0
 for item in os.listdir(current_dir):
@@ -64,14 +94,18 @@ for item in os.listdir(current_dir):
     if item.startswith("iteration_"):
         num_iterations += 1
 
+
 # create a new directory for the iteration
 current_dir = current_dir + "/" + "iteration_"+str(num_iterations)
 os.mkdir(current_dir)
 
+
 while keep_going:
     images = []
     for i in range(NUM_PER_GENERATION):
-        if ORIGINAL is not None or current_cppn is not None:
+        if MODIFY_IMAGE:
+            cppn = make_cppn(num_inputs=5, num_outputs=3, num_hnodes=NUM_HIDDEN, num_connections=NUM_CONNECTIONS)
+        elif ORIGINAL is not None or current_cppn is not None:
             cppn = load_cppn(current_cppn)
             cppn.evolve(add_node_amount=ADD_NODE_AMOUNT, add_edge_amount=ADD_EDGE_AMOUNT, edge_mutate_rate=EDGE_MUTATE_RATE, node_mutate_rate=NODE_MUTATE_RATE, prob_table=PROB_TABLE)
         else:
@@ -80,7 +114,20 @@ while keep_going:
             cppn = make_cppn(num_inputs=NUM_INPUTS, num_outputs=NUM_OUTPUTS, num_hnodes=NUM_HIDDEN, num_connections=NUM_CONNECTIONS)
         cppn_dir = current_dir + "/" + str(i)
         cppn.save(cppn_dir)
-        if MAP_OUTPUT_COORDS:
+        if MODIFY_IMAGE:
+            out_coords = run_cppn(cppn, inputs)
+            image = Image.new("RGB", (new_x, new_y))
+            for index, coord in enumerate(out_coords):
+                x = int(abs(inputs[index][0]*new_x))
+                y = int(abs(inputs[index][1]*new_y))
+                r = int(abs(coord[0] * 255))
+                g = int(abs(coord[1] * 255))
+                b = int(abs(coord[2] * 255))
+                image.putpixel((x, y), (r, g, b))
+                # resize the image to the 256x256
+                image = image.resize((256, 256), Image.ANTIALIAS)
+            images.append(image)
+        elif MAP_OUTPUT_COORDS:
             images.append(make_image_with_output_coords(cppn, IMAGE_X, IMAGE_Y, image_directory=current_dir, save=False, resize_x=256, resize_y=256))
         else:
             images.append(make_image(cppn, IMAGE_X, IMAGE_Y, image_directory=current_dir, save=False, resize_x=256, resize_y=256))
